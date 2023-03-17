9f04ff692be4fa4f268b623b8deedfa11775fd41
SBAITIAchraf <achraf.sbaiti@um6p.ma>
2023-03-17 15:25:16 +0100

    Create DynamicString.c

---
+++ DynamicString.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    int *arr;
+    int len;
+    int mal;
+}DynamicString;
+
+void *append(DynamicString *my_arr, int elem)
+{
+    if (my_arr->len == my_arr->mal)
+    {
+        my_arr->mal *= 2;
+        int *new_arr =(int *) malloc(my_arr->mal * sizeof(int));
+        for (int i = 0; i < my_arr->len; i++)
+        {
+            new_arr[i] = my_arr->arr[i];
+        }
+        free(my_arr->arr);
+        my_arr->arr = new_arr;
+    }
+    my_arr->arr[my_arr->len] = elem;
+    my_arr->len++;
+}
+
+int main()
+{
+
+    DynamicString array = {(int *) malloc(array.mal * sizeof(int)), 2, 4};
+    array.arr[0] =2;
+    array.arr[1] =47;
+
+    append(&array, 5);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[i]);
+    }
+    append(&array, 12);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[i]);
+    }
+    return 0;
+}
