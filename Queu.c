c4817e72ee0efe98dc296f15f2ad02d4f6515d3c
SBAITIAchraf <achraf.sbaiti@um6p.ma>
2023-03-18 21:29:56 +0100

    q

---
+++ Queu.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    int *arr;
+    int len;
+    int mal;
+    int start_index;
+    int empty;
+}queu;
+
+void *push(queu *my_arr, int elem)
+{
+    if (my_arr->len >= my_arr->mal)
+    {
+        int new_mal = my_arr->mal * 2;
+        int *new_arr =(int *) malloc(new_mal * sizeof(int));
+        for (int i = 0; i < my_arr->len; i++)
+        {
+            new_arr[(new_mal - 1) - i] = my_arr->arr[(my_arr->mal - 1) - i];
+        }
+        free(my_arr->arr);
+        my_arr->mal = new_mal;
+        my_arr->arr = new_arr;
+    }
+    my_arr->arr[my_arr->mal - my_arr->len - 1] = elem;
+    my_arr->len++;
+    my_arr->start_index = my_arr->mal - my_arr->len;
+}
+
+void *pop(queu *my_arr)
+{
+    if (my_arr->len != 0)
+    {
+        my_arr->len--;
+        if (my_arr->mal - my_arr->len >= my_arr->empty)
+        {
+            my_arr->empty *= 2;
+            my_arr->mal = my_arr->len;
+            int *new_arr = (int *) malloc(my_arr->len * sizeof(int));
+            for (int i = 0; i < my_arr->len; i++)
+            {
+                new_arr[i] = my_arr->arr[i];
+            }
+            free(my_arr->arr);
+            my_arr->arr = new_arr;
+        }
+    }
+}
+
+int main()
+{
+
+    queu array = {(int *) malloc(array.len * sizeof(int)), 7, 8, array.mal - array.len, 1};
+
+    for (int i = 0; i<array.len; i++)
+    {
+        array.arr[array.start_index + i] = i;
+    }
+    
+    push(&array, 5);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[array.start_index + i]);
+    }
+    printf("Stop\n\n");
+
+    push(&array, 12);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[array.start_index + i]);
+    }
+    printf("Stop\n\n");
+
+    /*pop(&array);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[i]);
+    }
+    printf("Stop\n\n");
+
+    pop(&array);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[i]);
+    }
+    printf("Stop\n\n");
+
+
+    push(&array, 254);
+    for (int i = 0; i<array.len; i++)
+    {
+        printf("%d\n", array.arr[i]);
+    }
+    printf("Stop\n\n");*/
+    return 0;
+}
