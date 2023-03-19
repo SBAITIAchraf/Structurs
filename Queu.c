be1adadc49bbbdb1c9086f50b1c426b17878bcc3
SBAITIAchraf <achraf.sbaiti@um6p.ma>
2023-03-19 15:28:52 +0000

    almost there

--- Queu.c
+++ Queu.c
@@ -3,11 +3,11 @@
 
 typedef struct 
 {
-    int *arr;
     int len;
     int mal;
     int start_index;
     int empty;
+    int *arr;
 }queu;
 
 void *push(queu *my_arr, int elem)
@@ -24,36 +24,36 @@ void *push(queu *my_arr, int elem)
         my_arr->mal = new_mal;
         my_arr->arr = new_arr;
     }
-    my_arr->arr[my_arr->mal - my_arr->len - 1] = elem;
     my_arr->len++;
     my_arr->start_index = my_arr->mal - my_arr->len;
+    my_arr->arr[my_arr->start_index] = elem;
 }
 
-/*void *pop(queu *my_arr)
+void *pop(queu *my_arr)
 {
     if (my_arr->len != 0)
     {
-        my_arr->mal--;
         my_arr->len--;
         if (my_arr->mal - my_arr->len >= my_arr->empty)
         {
             my_arr->empty *= 2;
-            my_arr->mal = my_arr->len;
-            int *new_arr = (int *) malloc(my_arr->len * sizeof(int));
+            int new_mal = my_arr->len;
+            int *new_arr = (int *) malloc(new_mal * sizeof(int));
             for (int i = 0; i < my_arr->len; i++)
             {
-                new_arr[i] = my_arr->arr[i];
+                new_arr[i] = my_arr->arr[my_arr->start_index + i];
             }
             free(my_arr->arr);
+            my_arr->mal = new_mal;
             my_arr->arr = new_arr;
         }
     }
-}*/
+}
 
 int main()
 {
 
-    queu array = {(int *) malloc(array.len * sizeof(int)), 7, 8, array.mal - array.len, 1};
+    queu array = { 7, 8, array.mal - array.len, 1, (int *) malloc(array.mal * sizeof(int))};
 
     for (int i = 0; i<array.len; i++)
     {
@@ -74,7 +74,7 @@ int main()
     }
     printf("Stop\n\n");
 
-    /*pop(&array);
+    pop(&array);
     for (int i = 0; i<array.len; i++)
     {
         printf("%d\n", array.arr[i]);
@@ -94,6 +94,6 @@ int main()
     {
         printf("%d\n", array.arr[i]);
     }
-    printf("Stop\n\n");*/
+    printf("Stop\n\n");
     return 0;
 }
